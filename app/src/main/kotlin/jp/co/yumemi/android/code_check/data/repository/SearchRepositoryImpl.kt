b66c20db009f7ab17a7228b2a09202619ef5cbb7
KazumaProject <59742125+KazumaProject@users.noreply.github.com>
2025-03-08 01:34:20 -0500

    implement search repository in domain and data layers

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/data/repository/SearchRepositoryImpl.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/data/repository/SearchRepositoryImpl.kt
@@ -1,20 +1,24 @@
 package jp.co.yumemi.android.code_check.data.repository
 
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.client.request.header
 import io.ktor.client.request.parameter
-import io.ktor.client.statement.HttpResponse
+import jp.co.yumemi.android.code_check.data.remote.GitHubSearchResult
+import jp.co.yumemi.android.code_check.data.remote.RepoSearchResponse
 import jp.co.yumemi.android.code_check.domain.repository.SearchRepository
 import javax.inject.Inject
 
 class SearchRepositoryImpl @Inject constructor(
     private val httpClient: HttpClient
 ) : SearchRepository {
-    override suspend fun searchRepositories(query: String): HttpResponse {
-        return httpClient.get("https://api.github.com/search/repositories") {
-            header("Accept", "application/vnd.github.v3+json")
-            parameter("q", query)
-        }
+    override suspend fun searchRepositories(query: String): List<RepoSearchResponse> {
+        val result: GitHubSearchResult =
+            httpClient.get("https://api.github.com/search/repositories") {
+                header("Accept", "application/vnd.github.v3+json")
+                parameter("q", query)
+            }.body()
+        return result.items
     }
 }
